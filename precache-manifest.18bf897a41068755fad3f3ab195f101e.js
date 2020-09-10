05aeb52eff66d19ced0a26b791ec95a73417004d
jbellmont <bellmont@gmail.com>
2020-09-10 21:05:06 +0100

    Updates

---
+++ precache-manifest.18bf897a41068755fad3f3ab195f101e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baac6145b3178c4d54094e42c3fba994",
+    "url": "/task-list-react/index.html"
+  },
+  {
+    "revision": "a7e8a21709bae844ea24",
+    "url": "/task-list-react/static/css/main.813a2a04.chunk.css"
+  },
+  {
+    "revision": "9a51a0b5337b1411853c",
+    "url": "/task-list-react/static/js/2.f525fb91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-list-react/static/js/2.f525fb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e8a21709bae844ea24",
+    "url": "/task-list-react/static/js/main.201799d1.chunk.js"
+  },
+  {
+    "revision": "99c452253ab3ac41f1f8",
+    "url": "/task-list-react/static/js/runtime-main.4d92fc26.js"
+  }
+]);
